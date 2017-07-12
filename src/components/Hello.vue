45cf778c2c2f448e23c2b13f90339f253c380805
mcc <2529443680@qq.com>
2017-07-12 18:03:47 +0800

    version3

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,7 +17,6 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
-    <router-link to="/"><div style="width:30%;height:20px;background:#ccc"></div></router-link>
   </div>
 </template>
 
---
+++ src/components/buttoncounter.vue
@@ -0,0 +1,27 @@
+<template>
+<button v-on:click="increment">{{counter}}</button>
+</template>
+<script>
+export default {
+    name: 'buttoncounter',
+    data() {
+        return {
+        	counter:0
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+        /*$(function(){
+        	console.log('jquery success');
+        })*/
+    },
+    methods: {
+    	increment:function(){
+    		this.counter=this.counter+=1;
+    		this.$emit('increment');
+    	}
+    }
+}
+</script>
