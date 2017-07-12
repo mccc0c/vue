9431f140146b33db205452d7a7cf0f3864ebac9a
mcc <2529443680@qq.com>
2017-07-12 15:07:38 +0800

    version1

---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="header">
+		<router-link to="/"><div style="width:30%;height:20px;background:#ccc"></div></router-link>
+		<router-link to="/hello"><div style="width:30%;height:20px;background:#ddd"></div></router-link>
+	</div>
+</template>
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+     
+    }
+  },
+  created (){
+
+  },
+  mounted (){
+  	console.log('jquery success');
+  	/*$(function(){
+
+  	})*/
+  },
+  method:{
+
+  }
+}
+</script>
+<style scoped>
+	
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import $ from 'jquery'
+import '../static/js/easy.js'
+import '../static/js/jquery.validate.js'
+import '../static/css/easy.css'
+
+/*可导入公用的css和js*/
+/*import jQuery from 'jQuery'*/
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+/*new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
-})
+})*/
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app-box')
